23fbde91785044ba568c71ed4c405c98d721df2c
Cameron Hopkins <hopkins_cameron@yahoo.com>
2017-07-27 15:01:30 -0400

    Pushing large changes

---
+++ modules/core/client/services/client.service.js
@@ -0,0 +1,32 @@
+﻿angular.module('app').service('Service', ['DataAccessFactory', function (DataAccessFactory) {
+    // set up the service
+    var service = {};
+
+    // the current path (after the hash)
+    service.afterPath = "";
+
+    // the app name
+    service.appName = "";
+
+    // set the application name
+    service.setAppName = function () {
+        // get app name
+        return DataAccessFactory.getAppName().then(function (responseAN) {
+            service.appName = responseAN.data.appName;
+        })
+        .catch(function (responseAN) {
+            service.appName = "Cameron Hopkins";
+        });
+    };
+
+    // shortens url
+    service.shortenURL = function (url) {
+        // shorten the url
+        return DataAccessFactory.shortenURL(url)
+        .catch(function (responseU) {
+            return { "error": true, "title": responseU.data.title, "status": responseU.status, "message": responseU.data.message };
+        });
+    };
+
+    return service;
+}]);
---
+++ modules/core/client/services/data-access.client.service.js
@@ -0,0 +1,52 @@
+﻿angular.module('app').factory('DataAccessFactory', ['$http', '$location', function ($http, $location) {
+    // set up the factory
+    var factory = {};
+    var appPath = $location.$$absUrl.split('#')[0] + 'api';
+
+    // gets app name
+    factory.getAppName = function () {
+        // set the endpoint
+        var endpoint = appPath + "/appName";
+
+        // create request
+        var req = {
+            method: 'GET',
+            url: endpoint,
+            headers: {
+                'Content-Type': 'application/json; odata=verbose',
+                'Accept': 'application/json; odata=verbose'
+            },
+            data: undefined
+        };
+
+        // send request
+        return $http(req);
+    };
+
+    // shortens url
+    factory.shortenURL = function (url) {
+        // set the endpoint
+        var endpoint = appPath + "/shortenUrl";
+
+        // stringify the url data
+        var urlData = JSON.stringify({
+            "longUrl": url
+        });
+
+        // create request
+        var req = {
+            method: 'POST',
+            url: endpoint,
+            headers: {
+                'Content-Type': 'application/json; odata=verbose',
+                'Accept': 'application/json; odata=verbose'
+            },
+            data: urlData
+        };
+
+        // send request
+        return $http(req);
+    };
+
+    return factory;
+}]);
